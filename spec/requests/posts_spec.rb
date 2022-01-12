c07fba1190cca369c07442d64be64be82fd767bb
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-12 15:10:10 +0100

    fixing rubocop errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    before(:example) { get("/users/100/posts")}
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get('/users/100/posts') }
 
-    it "http response should be sucess" do
-      
+    it 'http response should be sucess' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders correct page contents" do
-      
+    it 'renders correct page contents' do
       expect(response.body).to include('Show all posts')
     end
 
-    it "renders index template" do
+    it 'renders index template' do
       expect(response).to render_template('index')
     end
   end
 
-  describe "GET /show" do
-    before(:example) { get("/users/111/posts/152")}
-    it "http response should be sucess" do
-      
+  describe 'GET /show' do
+    before(:example) { get('/users/111/posts/152') }
+    it 'http response should be sucess' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders correct page contents" do
-      
+    it 'renders correct page contents' do
       expect(response.body).to include('Show single post details')
     end
 
-    it "renders index template" do
+    it 'renders index template' do
       expect(response).to render_template('show')
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,36 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    before(:example) { get("/users")}
-    it "http response should be sucess" do
-      
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get('/users') }
+    it 'http response should be sucess' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders correct page contents" do
-      
+    it 'renders correct page contents' do
       expect(response.body).to include('List all Users')
     end
 
-    it "renders index template" do
+    it 'renders index template' do
       expect(response).to render_template('index')
     end
   end
 
-  describe "GET /show" do
-    before(:example) { get("/users/10")}
-    it "http response should be sucess" do
-      
+  describe 'GET /show' do
+    before(:example) { get('/users/10') }
+    it 'http response should be sucess' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders correct page contents" do
-      
+    it 'renders correct page contents' do
       expect(response.body).to include('Show single user details')
     end
 
-    it "renders index template" do
+    it 'renders index template' do
       expect(response).to render_template('show')
     end
   end
