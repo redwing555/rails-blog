f63b103139fdca3f6f2014381e4f2b620a8cd283
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-12 15:03:05 +0100

    adding tests for get show action for users and posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    before(:example) { get user_posts_path(1)}
+    before(:example) { get("/users/100/posts")}
 
     it "http response should be sucess" do
       
@@ -16,6 +16,23 @@ RSpec.describe "Posts", type: :request do
 
     it "renders index template" do
       expect(response).to render_template('index')
+    end
+  end
+
+  describe "GET /show" do
+    before(:example) { get("/users/111/posts/152")}
+    it "http response should be sucess" do
+      
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders correct page contents" do
+      
+      expect(response.body).to include('Show single post details')
+    end
+
+    it "renders index template" do
+      expect(response).to render_template('show')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    before(:example) { get users_path(1)}
+    before(:example) { get("/users")}
     it "http response should be sucess" do
       
       expect(response).to have_http_status(:ok)
@@ -15,6 +15,23 @@ RSpec.describe "Users", type: :request do
 
     it "renders index template" do
       expect(response).to render_template('index')
+    end
+  end
+
+  describe "GET /show" do
+    before(:example) { get("/users/10")}
+    it "http response should be sucess" do
+      
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders correct page contents" do
+      
+      expect(response.body).to include('Show single user details')
+    end
+
+    it "renders index template" do
+      expect(response).to render_template('show')
     end
   end
 end
