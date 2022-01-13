0cf4183eb48fdd75da6d928085b4ad2e41b79021
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-13 14:42:54 +0100

    run migrate to create schema

--- db/migrate/20220113133644_add_user_post_join_table.rb
+++ db/migrate/20220113133644_add_user_post_join_table.rb
@@ -1,7 +1,10 @@
 class AddUserPostJoinTable < ActiveRecord::Migration[7.0]
   def change
-    create_join_table :users, :posts, table_name :likes do |t|
-      t.index [:user_id, :post_id], unique: true
+    create_join_table :users, :posts, table_name: :likes do |t|
+      t.index :user_id, unique: true
+      t.index :post_id, unique: true
+
+      t.timestamps
     end
   end
 end
