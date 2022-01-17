dcf38c3b0750bca9bddaa8a8e593fa21d6456989
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-17 15:15:19 +0100

    fixing linters erorrs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User'
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
 
-    def comments_counter
-        post.increment!(:comments_counter)
-    end
+  def comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-    has_many :likes
-    has_many :comments
+  has_many :likes
+  has_many :comments
 
-    belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User'
 
-    def post_counter
-        author.increment!(:posts_counter)
-    end
+  def post_counter
+    author.increment!(:posts_counter)
+  end
 
-    def recent_comments
-        comments.limit(5).oder(created_at: :desc)
-    end
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
