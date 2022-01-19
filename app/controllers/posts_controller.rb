b97f326edba93d6cfe220c243053e5a5219b883d
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-19 17:35:22 +0100

    adding show controller action for single post +adding show action view for single+ seeding database with likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
+
+  def show
+    @user = User.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:id])
+    # @post = @user.posts.find_by_id(params[:id])
+  end
+  
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
   end
 
-  def show; end
+  
 end
