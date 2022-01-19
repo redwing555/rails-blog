f4a768fed1df88d1648f286b1b110bee10735a98
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-19 17:41:28 +0100

    fixing linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,11 @@
 class PostsController < ApplicationController
-
   def show
-    @user = User.find_by_id(params[:id])
+    @user = User.find_by_id(params[:user_id])
     @post = Post.find_by_id(params[:id])
-    # @post = @user.posts.find_by_id(params[:id])
   end
-  
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc)
   end
-
-  
 end
