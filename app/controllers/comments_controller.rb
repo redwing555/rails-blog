5c67b28abcf738c254604bcaf11759a041b5c5b2
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-20 13:25:36 +0100

    adding controller action for comments + view form file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.post_id = params[:post_id]
+        @comment.author_id = current_user.id
+        if @comment.save
+            flash[:notice] = 'Comment created!'
+            redirect_to user_posts_path(current_user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def comment_params
+        params.permit(:exit)
+    end
 end
