8e253d5d21c2e46e574aeec9781bc1ed3e0381e7
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-20 20:08:56 +0100

    adding new to comments ressources

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :comments, only: %i[create]
+    resources :comments, only: %i[new create]
     resources :likes, only: %i[create]
   end
 end
