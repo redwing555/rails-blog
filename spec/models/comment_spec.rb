2a52b3fbfa6fbcee6c3385bb017468fa0dde6e26
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-21 09:57:34 +0100

    writing spec tests for comment model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'comment model methods' do
+    subject do
+      Comment.new
+    end
+
+    before {subject.save}
+
+    it "title presence" do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
