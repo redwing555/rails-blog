3598d207df69e177b133ccafac897af41f6171a6
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-21 09:41:22 +0100

    writing spec tests for post model validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject do
+      Post.new
+    end
+
+    before {subject.save}
+
+    it "title presence" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "title should not exceed 250 char" do
+      subject.title = "post 1"*251
+      expect(subject).to_not be_valid
+    end
+
+    it "comments counter should be integer " do
+      subject.comments_counter = 1.7
+    expect(subject).to_not be_valid
+    end
+
+    it "comments counter should be greater or equal to 0 " do
+      subject.comments_counter = -1
+    expect(subject).to_not be_valid
+    end
+
+    it "Likes counter should be integer " do
+      subject.likes_counter = 1.7
+    expect(subject).to_not be_valid
+    end
+
+    it "Likes counter should be greater or equal to 0 " do
+      subject.likes_counter = -1
+    expect(subject).to_not be_valid
+    end
+  end
 end
